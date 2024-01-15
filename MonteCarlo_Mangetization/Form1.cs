0d76d61cde25e5bbc5ec7e8c1e3a65d1f979a599
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2024-01-15 20:20:23 +0500

    Add files via upload

---
+++ MonteCarlo_Mangetization/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonteCarlo_Mangetization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MonteCarlo mc = new MonteCarlo(this);
+
+            int n_sweeps = 100;//at every temperature value, spin system will be swept
+            //n_sweep times
+
+            double M = 0, Mave = 0, Tc = 0;//M is magnetization during one sweep
+            //Mave is average magnetization of all sweeps
+
+            bool check;
+
+            //frm2.Show();
+            for (double T = 0.1; T < 10; T = T + 0.2)//Temperature loop for MFT as well as Monte Carlo
+            {
+                Mave = 0;
+                for (int sweep = 0; sweep < n_sweeps; sweep++)//sweep loop
+                {
+                    M = 0;//at the start of each sweep, M is taken zero
+                    for (int i = 1; i < (mc.n_spins - 1); i++)
+                        //both for loops represent
+                        //single sweep
+                    {
+                        for (int j = 1; j < (mc.n_spins - 1); j++)
+                        {
+                            mc.E1 = mc.EnergyCal(i, j);
+                            //it computes internal energy 
+                            //of spin at ith row and jth column, before its flipping
+
+                            //now assumingly flip the spin
+                            mc.spinsystem[i, j] = mc.spinsystem[i, j] * -1;//flip the spin
+                            mc.E2 = mc.EnergyCal(i, j);//it computes internal energy 
+                                                       //of spin at ith row and jth column, after its flipping
+
+                            check = mc.Flip(T);//check wheter spin flipping
+                            //is accepted or not
+                            if (check == true)
+                            {
+                                //accept the flip
+                                if (mc.spinsystem[i, j] == -1)//if spin-down
+                                    mc.gg.FillEllipse(mc.bred, 50 + j * 10, 200 + i * 10, 8, 8);
+                                else//if spin-up
+                                    mc.gg.FillEllipse(mc.bblue, 50 + j * 10, 200 + i * 10, 8, 8);
+                            }
+                            else//check==false means flipping is rejected
+                            {
+                                mc.spinsystem[i, j] = mc.spinsystem[i, j] * -1;
+                            }
+                            M = M + mc.spinsystem[i, j];//
+                        }//internal for loop
+                    }//external for loop
+                    Mave = Mave + M / (mc.n_spins * mc.n_spins - 4 * mc.n_spins + 4);
+                }//end of sweeps loop
+                Mave = Mave / n_sweeps;//average magnetization at single value
+                //of temperature
+                //**********MFT Computations for the average magnetization******
+
+                for (double s = 0; s < 1.5; s = s + 0.01)
+                {
+                    if (Math.Abs(s - Math.Tanh(mc.J * mc.Z * s / (mc.KB * T))) < 0.001)
+                        mc.gg1.FillEllipse(mc.bred, 350 + (float)T * 100,
+                        400 - (float)s * 300, 8, 8);
+                }
+
+                mc.gg1.FillEllipse(mc.bblue, 350 + (float)T * 100,
+                    400 - (float)Mave * 300, 8, 8);
+                if (Math.Abs(Mave) < 0.01)
+                { 
+                    Tc = T; 
+                }
+                //graph of average magnetization versus T
+            }//Temperature Loop
+            MessageBox.Show("Tc=" + Tc.ToString());
+        }
+    }
+    
+}
---
+++ MonteCarlo_Mangetization/MonteCarlo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo_Mangetization
+{
+    internal class MonteCarlo
+    {
+            //data variables
+            public Graphics gg, gg1;
+            public SolidBrush bred, bblue;
+            public Pen pblue, pred;
+            public Font f;
+
+            //take two dimensional array to represent two dimensional spin system
+            public int[,] spinsystem;
+            public int n_spins = 21;
+
+            bool test;
+            Random obj;
+
+            public double E1, E2, E_Flip, P_Flip, r, J, KB, Z, mu, H;
+
+            public MonteCarlo(Form1 frm1)
+            {
+                gg = frm1.CreateGraphics();
+                gg1 = frm1.CreateGraphics();
+                bred = new SolidBrush(Color.Red);
+                bblue = new SolidBrush(Color.Blue);
+                pblue = new Pen(Color.Blue, 5);
+                pred = new Pen(Color.Red, 5);
+                f = new Font("Arial", 16);
+
+                spinsystem = new int[n_spins, n_spins];
+                obj = new Random();
+                test = false;
+
+                E1 = 0; E2 = 0; E_Flip = 0; P_Flip = 0; r = 0; J = 1; KB = 1;
+                mu = 1; Z = 4; H = 0;
+
+                //show the spin system
+                for (int i = 0; i < n_spins; i++)
+                {
+                    for (int j = 0; j < n_spins; j++)
+                    {
+                        r = obj.NextDouble();
+                        if (r <= 0.5)
+                        {
+                            spinsystem[i, j] = 1;//spin up
+                            gg.FillEllipse(bblue, 50 + j * 10, 200 + i * 10, 8, 8);
+                        }
+                        else
+                        {
+                            spinsystem[i, j] = +1;//spin up
+                            gg.FillEllipse(bblue, 50 + j * 10, 200 + i * 10, 8, 8);
+                        }
+                    }
+                }//spin system shown
+            }//end of constructor
+             //other functions
+
+            public double EnergyCal(int r, int c)
+            {
+                return (-(int)J * spinsystem[r, c] * (spinsystem[r - 1, c]
+                    + spinsystem[r + 1, c] +
+                spinsystem[r, c - 1] + spinsystem[r, c + 1] + mu * H));
+            }
+
+            public bool Flip(double T)
+            {
+                E_Flip = E2 - E1;
+                if (E_Flip <= 0)
+                {
+                    test = true;//spin will flip
+                }
+                else//E_flip>0
+                {
+                    P_Flip = Math.Exp(-E_Flip / (KB * T));//probability of flipping
+                    r = obj.NextDouble();//random probability
+                    if (P_Flip >= r)
+                    {
+                        test = true;
+                    }
+                    else
+                    {
+                        test = false;
+                    }
+                }
+                return test;
+            }
+    }
+}
